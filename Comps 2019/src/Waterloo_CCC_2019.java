ac2644381410c64f68a37693d748821701dcfaac
JJ Chang <53491467+JJ-Chang@users.noreply.github.com>
2021-03-04 05:32:37 -0500

    Add files via upload

---
+++ Comps 2019/src/Waterloo_CCC_2019.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class Waterloo_CCC_2019 {
+
+	public static void Main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter a int");
+		int num = in.nextInt();
+		System.out.println("Enter a double");
+		double num2 = in.nextDouble();
+
+		char x = (char) num;
+		System.out.println("X = " + x);
+
+		x = (char) num2;
+		System.out.println("X = " + x);
+
+		in.close();
+	}
+
+	public static void Testing(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+
+		//Winning Score
+
+		/*int apple3, apple2, apple1, banana3, banana2, banana1;
+			int appleScore, bananaScore;
+
+			apple3 = in.nextInt();
+			apple2 = in.nextInt();
+			apple1 = in.nextInt();
+			banana3 = in.nextInt();
+			banana2 = in.nextInt();
+			banana1 = in.nextInt();
+
+			appleScore = (apple3 * 3) + (apple2 * 2) + (apple1);
+			bananaScore = (banana3 * 3) + (banana2 * 2) + (banana1);
+
+			if (appleScore > bananaScore) {
+				System.out.println("A");
+			}
+			else if (bananaScore > appleScore) {
+				System.out.println("B");
+			}
+			else if (appleScore == bananaScore) {
+				System.out.println("T");
+			}
+		 */
+
+		//Time to Decompress
+
+		/*int L, N;
+			String x;
+
+			System.out.println("test");
+
+			L = in.nextInt();
+			for(int i = 0; i <= L; i++) {
+				N = Integer.parseInt(in.nextLine());
+				x = in.nextLine();
+			}
+
+			System.out.println("test2");
+			//why doesn't it wait for the input		 
+		 */
+
+		//Flipper
+		/*int Q1, Q2, Q3, Q4, lengthIns;
+			String ins;
+			Q1 = 1;
+			Q2 = 2;
+			Q3 = 3;
+			Q4 = 4;
+
+			System.out.println("test");
+
+			ins = in.nextLine();
+			lengthIns = String.
+		 */
+
+		in.close();
+	}
+}
